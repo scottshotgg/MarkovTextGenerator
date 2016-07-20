1b3bac581bdcc6720bec143237d986318d0dc827
scottshotgg <scgaydos@gmail.com>
2016-07-20 00:32:14 -0500

    Order 2 now, sentences are a bit more coherent, but still do not make sense most of the time. Will probably template this shit and make it order-n later, added a War And Peace file that has the newlines removed.

--- markov.py
+++ markov.py
@@ -1,13 +1,16 @@
 import random
 
 
-sample = open('WarAndPeace')
+sample = open('WarAndPeaceRemoved')
 
 sampleText = sample.read()
 
 #print sampleText
 
-sampleText = sampleText.replace('\n', ' ')
+#sampleText = sampleText.replace('\n', ' ')
+#WarAndPeaceRemoved = open('WarAndPeaceRemoved', "w")
+#WarAndPeaceRemoved.write(sampleText)
+#WarAndPeaceRemoved.close()
 
 words = sampleText.split(' ')
 
@@ -21,16 +24,7 @@ for word in words:
 
 dict = {}
 
-word = 'me'
-word2 = 'you'
-
-dict[word + word2] = 'this'
-
-print dict
-
-
-
-i = 0
+i = 1
 #print len(words)
 
 state = 'post'
@@ -39,10 +33,10 @@ if state is 'post':
 	for word in words:
 		#print "word: " + word
 		if i != len(words) - 1:
-			if word in dict:
-				dict[word] += [words[i + 1]]
+			if (words[i - 1], words[i]) in dict:
+				dict[(words[i - 1], words[i])] += [words[i + 1]]
 			else:
-				dict[word] = [words[i + 1]]
+				dict[(words[i - 1], words[i])] = [words[i + 1]]
 			i += 1
 			#print i
 
@@ -62,16 +56,31 @@ elif state is 'pre':
 
 	#print dict[word]
 word = random.choice(dict.keys())
-print '\n\n' + word + '\n\n'
+#print '\n\n' + '%s \n\n' % (word,)
+wordSave = word[1]
+print wordSave
 
+word2 = None
 strToPrint = ''
-for x in range (0, 50):
-	strToPrint += word + ' '
+for x in range (0, 1000):
+	strToPrint += '%s ' % (word[1],)
 	#print word
-	array = dict[word]
+	if word2 is not None:
+		print "passed the if"
+		try:
+			array = dict[(word2, wordSave)]
+		except KeyError as err:
+			#strToPrint += " * RANDOM * " + "(" + word2 + ", " + wordSave + ")"
+			array = dict[random.choice(dict.keys())] 
+	else:
+		print "didnt pass the if"
+		array = dict[word]
 	#print array
 	randvar = random.randint(0, len(array) - 1)
-	word = array[randvar]
-
+	word2 = word[1]
+	wordSave = array[randvar]
+	print ("(" + word2 + ", " + wordSave + ")")
+	word = (word2, wordSave)
 
 print strToPrint
+sample.close()
